c8113a7b5f34c89ca5bc5bd4bfc83a4d88353cbf
Matias Rivera <matias.g.rivera.civ@gmail.com>
2024-06-14 11:30:05 -0700

    Add files via upload
    Finally got my addRows function to work in a consistent way within the BuildATable.js file.

---
+++ BeanCounting.js
@@ -0,0 +1,18 @@
+function countBs(str) {
+    bCount = 0;
+    for (let i = 0; i < length.str; i++) {
+        if (str[i] === 'B') {
+            bCount+=1;
+        }
+    }
+    console.log(bCount);
+};
+
+function countChar (str, x) {
+    xCount = 0;
+    for (let i = 0; i < length.str; i++) {
+        if (str[i] === x) {
+            xCount+=1
+        }
+    }
+}
